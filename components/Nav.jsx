c820ce546b34ceaa7ac4e4895ae238ab25e97b9d
Felipe Torres <fetl9239@ufmg.br>
2024-03-31 20:17:47 -0300

    fix: fix navbar logo Link component

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -19,19 +19,25 @@ const Nav = () => {
                     className="object-contain"
                 />
                 <p className="logo_text">Promptopia</p>
-
-                {/* Mobile Navigation */}
-                <div className="sm:flex hidden">
-                    {isUserLoggedIn ? (
-                        <div>
-
-                        </div>
-                    ) : (
-                        <>
-                        </>
-                    )}
-                </div>
             </Link>
+
+            {/* Desktop Navigation */}
+            <div className="sm:flex hidden">
+                {isUserLoggedIn ? (
+                    <div className="flex gap-3 md:gap-5">
+                        <Link href="/create-prompt" className="black_btn">
+                            Create Post
+                        </Link>
+
+                        <button type="button" onClick={signOut} className="outline_btn">
+                            Sign Out
+                        </button>
+                    </div>
+                ) : (
+                    <>
+                    </>
+                )}
+            </div>
         </nav>
     )
 }
